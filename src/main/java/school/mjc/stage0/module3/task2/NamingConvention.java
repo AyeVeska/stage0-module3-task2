f09893bad18795f6bcd030d1ff3907cb511989aa
Ferid <feridagazade157@gmail.com>
2025-06-19 15:36:39 +0400

    2nd commit

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variableTen = 10;
-        int variable_one = 1;
+        int variable_One = 1;
         int variableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
+        int variable_Three = 3;
+        final int VARIABLEFOUR = 4;
+        final int VARIABLEFIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
